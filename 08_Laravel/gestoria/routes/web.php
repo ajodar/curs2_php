871704feee34f5c058dcc77847931ecd9cb0093e
ajodar <69302933+ajodar@users.noreply.github.com>
2021-01-15 10:38:42 +0100

    Update

--- 08_Laravel/gestoria/routes/web.php
+++ 08_Laravel/gestoria/routes/web.php
@@ -24,7 +24,7 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
 
     Route::group(['middleware' => 'auth'], function () {
 
-    Route::get('/home', 'HomeController@index')->name('home');
+        Route::get('/home', 'HomeController@index')->name('home');
 
     });
 
